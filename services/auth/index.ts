f929594806f3c09cf1a9829007e4014aeb5cff6a
lucas.dalan <lucas.dalan@frgnet.com.br>
2024-03-14 14:25:53 -0300

    commit

---
+++ services/auth/index.ts
@@ -0,0 +1,59 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../database";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: String(credentials?.email) },
+        });
+
+        if (!user) {
+          throw new Error("Invalid Credentials");
+        }
+
+        if (user.password != credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/dashboard",
+    signOut: "/",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
